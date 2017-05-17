6aa4391582bc31bc699fadb7388e4c69467a1149
Stephan H. Wissel <stw@linux.com>
2017-05-18 00:21:24 +0800

    Raw layout no script code yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { NewMemberComponent } from './new-member/new-member.component';
+import { MemberListComponent } from './member-list/member-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewMemberComponent,
+    MemberListComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
